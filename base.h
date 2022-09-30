0f86c38fb4b0a5322b12200b75f657165a9be0a0
wordqiong <1592851500@qq.com>
2022-09-30 17:40:12 +0800

    '多行注释没做完，其他的可以了'

---
+++ base.h
@@ -0,0 +1,33 @@
+#include<iostream>
+
+
+/****************************************
+ * base 类 
+ * 完成基本的类型判断  
+ * ***************************************/
+class base{
+public:
+
+
+    virtual ~base();
+public:
+    int charKind(char c );//判断输入字符类型 是 数字 字母 还是 其他符号 状态机使用
+    int wordWrongAnalysis(char str[],int type);//错误判断
+    int isDelimiter(char c );//界符 可以用来删除空格
+protected:
+    
+    int isInt(char str[]);//整型
+    int isFloat(char str[]);//float 型
+
+    int isSignWord(char str[]);// 标识符 
+    int isKeyWord(char str[]);//保留字  关键字 
+
+
+    int isNum(char c);//是不是数字
+    int isChar(char c);//char
+
+    int isSpecialSign(char c);//看标识符命名是否正确
+
+   
+
+};
